2464793c35b23847559f8cb7075a5459f42d7599
KimInKyeom <freeartjin@naver.com>
2023-06-04 23:03:09 +0900

    스프링 입문(김영한) - 섹션 1.프로젝트 환경설정

---
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,17 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    /*
+    *  웹 애플리케이션에서 /hello라고 들어오면 이 메소드를 호출
+    * */
+    @GetMapping("hello")
+    public String hello(Model model){
+        model.addAttribute("data", "hello!!");
+        return "hello";
+     }
+}
